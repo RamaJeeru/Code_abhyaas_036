f9f7daa1cccfa2a78b63124450775c58667d6881
RamaJeeru <110540606+RamaJeeru@users.noreply.github.com>
2023-09-01 15:41:57 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,35 @@
+public class RemoveDuplicates{
+
+    static int removeduplis(int[] arr){
+        
+        int rd = 0;
+        for (int i = 1; i < arr.length; i++) {
+
+            if(arr[rd] != arr[i]){
+                rd++;
+                arr[rd] = arr[i];
+            }
+            
+        }
+        return rd+1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,2,2,3,4,5,5,5};
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        int rd = removeduplis(arr);
+        System.out.println();
+
+        for (int i = 0; i < rd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        
+
+    }
+}
