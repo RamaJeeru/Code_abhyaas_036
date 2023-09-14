3a5c7f843031f73652c1613812e0aa06c1a05fa4
RamaJeeru <110540606+RamaJeeru@users.noreply.github.com>
2023-09-14 15:40:42 +0530

    Add files via upload

---
+++ InverseOfArray.java
@@ -0,0 +1,23 @@
+class InverseOfArray{
+    static int[] inverse(int[] a){
+
+        int[] b = new int[a.length];
+        for(int i=0;i<a.length;i++){
+            int v = a[i];
+            b[v] = i;
+        }
+        return b;
+    }
+    public static void main(String[] args){
+        int[] a = {2,3,1,0,4};
+        for (int i : a) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        int[] b = inverse(a);
+        
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i]+" ");
+        }
+    }
+}
